cc4435a805a5679471f0dcf3909a353bfbff5336
mustafa <mmustafayev0001@gmail.com>
2025-05-13 18:20:19 +0400

    add consul support

--- src/main/java/com/fintech/banking/common/aop/RequestLoggingAspect.java
+++ src/main/java/com/fintech/banking/common/aop/RequestLoggingAspect.java
@@ -8,7 +8,6 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.context.request.RequestContextHolder;
